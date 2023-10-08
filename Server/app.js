15c46a406febc6e8202b1d43c23763e041543381
Pablo Porta <100687198+DarkProgrammerEs@users.noreply.github.com>
2023-10-08 09:58:40 +0200

    User interface improved

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,19 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname + '/public')
 app.set("View Engine", "ejs")
 
 app.get("/", (req, res)=>{
-    console.log("Here");
+    console.log("/");
     res.render("../Client/index.ejs")
+});
+
+app.get("/new", (req, res)=>{
+    console.log("/new");
+    res.render("../Client/new.ejs")
 });
 
 app.listen(3000)
